9b2f78d39accdd137df6d278b095993d5e01541f
LukassF <szkulkerolf@gmail.com>
2024-09-23 16:58:42 +0200

    add rive

---
+++ public/rive.js
@@ -0,0 +1,28 @@
+import * as rive from "@rive-app/canvas";
+
+const chatterRive = new rive.Rive({
+  src: "../assets/chat.riv",
+  canvas: document.getElementById("canvas_chat"),
+  autoplay: true,
+  onLoad: () => {
+    chatterRive.resizeDrawingSurfaceToCanvas();
+  },
+});
+
+const spotifyRive = new rive.Rive({
+  src: "../assets/spotify.riv",
+  canvas: document.getElementById("canvas_spotify"),
+  autoplay: true,
+  onLoad: () => {
+    spotifyRive.resizeDrawingSurfaceToCanvas();
+  },
+});
+
+const nightlyRive = new rive.Rive({
+  src: "../assets/coins.riv",
+  canvas: document.getElementById("canvas_nightly"),
+  autoplay: true,
+  onLoad: () => {
+    nightlyRive.resizeDrawingSurfaceToCanvas();
+  },
+});
